bea8ba105ed08b340ec877430af79fbade3e39a6
nazarisabbot <n.updesign@gmail.com>
2023-01-02 23:21:26 +0600

    Layout 2 section

--- src/modules/burgermenu.js
+++ src/modules/burgermenu.js
@@ -1,20 +1,10 @@
 const burger = document.querySelector(".header__burger");
 const header = document.querySelector(".header");
-const headerMenu = document.querySelector(".header__menu");
 const headerMenuitem = document.querySelector(".header__menuitem");
-const headerLinks = document.querySelectorAll(".header__link");
 
 burger.addEventListener("click", () => {
   document.body.classList.toggle("lock");
   header.classList.toggle("active");
-  headerMenu.classList.toggle("active");
   headerMenuitem.classList.toggle("active");
   burger.classList.toggle("active");
-  addClassActiveLi();
 });
-
-function addClassActiveLi() {
-  headerLinks.forEach((item) => {
-    item.classList.toggle("active");
-  });
-}
