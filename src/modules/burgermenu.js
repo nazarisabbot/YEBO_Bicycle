029edde053b015db4c6a00e3ed732001a02fd4cd
nazarisabbot <n.updesign@gmail.com>
2023-01-08 20:05:17 +0600

    Reconstraction

--- src/modules/burgermenu.js
+++ src/modules/burgermenu.js
@@ -1,10 +1,8 @@
 const burger = document.querySelector(".header__burger");
-const header = document.querySelector(".header");
-const headerMenuitem = document.querySelector(".header__menuitem");
+const headerBody = document.querySelector(".header__body");
 
-burger.addEventListener("click", () => {
-  document.body.classList.toggle("lock");
-  header.classList.toggle("active");
-  headerMenuitem.classList.toggle("active");
+burger.addEventListener("click", (burgerMenuOpenHandler) => {
   burger.classList.toggle("active");
+  headerBody.classList.toggle("active");
+  document.body.classList.toggle("lock");
 });
